f833c18b0cce40e4b8f82abbd6c0d3e844b9775e
upzzq <34890233+upzzq@users.noreply.github.com>
2018-11-07 11:38:54 +0800

    添加了druid数据源、添加了swagger配置

---
+++ src/main/java/com/hk/common/base/config/JacksonConfig.java
@@ -0,0 +1,38 @@
+package com.hk.common.base.config;
+
+import java.io.IOException;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+@Configuration
+public class JacksonConfig {
+	
+	@Bean
+	@Primary
+	@ConditionalOnMissingBean(ObjectMapper.class)
+	public ObjectMapper jacksonObjectMapper() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		// objectMapper.setSerializationInclusion(Include.NON_NULL);
+		objectMapper.getSerializerProvider().setNullValueSerializer(new JsonSerializer<Object>() {
+			@Override
+			public void serialize(Object value, JsonGenerator jsonGenerator, SerializerProvider serializerProvider)
+				throws IOException, JsonProcessingException {
+				//对象转换成json时, null转换为 ""
+				jsonGenerator.writeString("");
+				serializerProvider.getDefaultNullKeySerializer();
+			}
+		});
+		objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+		return objectMapper;
+	}
+}
---
+++ src/main/java/com/hk/common/base/config/Swagger2Config.java
@@ -0,0 +1,56 @@
+package com.hk.common.base.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class Swagger2Config {
+
+	@Value("${spring.application.name}")
+    public String appName;
+	
+	 @Bean
+    public Docket createRestApi() {
+       /* ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<Parameter>();
+        tokenPar.name("Authorization").defaultValue("Bearer ").description("jwt令牌").modelRef(new ModelRef("string")).parameterType("header").required(true).build();
+        pars.add(tokenPar.build());
+        return (new Docket(DocumentationType.SWAGGER_2)).apiInfo(this.apiInfo()).select().paths(PathSelectors.any()).build().globalOperationParameters(pars);*/
+       
+        return (new Docket(DocumentationType.SWAGGER_2))
+        		.apiInfo(this.apiInfo())
+        		.select()
+        		.paths(PathSelectors.any())
+        		.build();
+    }
+
+    private ApiInfo apiInfo() {
+        //return (new ApiInfoBuilder()).title("[" + this.appName + "] RESTful APIs").description("开发测试接口文档").termsOfServiceUrl("http://uinpay.com/").version("1.0").build();
+    	return new ApiInfoBuilder()
+    			//页面标题
+    			.title("[" + this.appName + "] RESTful APIs")
+    			//创建人
+    			.contact(new Contact("hkzzq", "www.baidu.com", "hk123@gmail.com"))
+    			//版本号
+    			.version("1.0")
+    			//描述
+    			.description("接口文档")
+    			.build();
+    }
+}
