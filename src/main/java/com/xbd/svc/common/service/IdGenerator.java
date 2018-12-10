96f8e2cedab896b17eb25fbbb0ba5d4f62af2008
upzzq <34890233+upzzq@users.noreply.github.com>
2018-12-10 18:14:03 +0800

    SnowflakeIdWorker 分布式id生成器

---
+++ src/main/java/com/xbd/svc/common/service/IdGenerator.java
@@ -0,0 +1,11 @@
+package com.xbd.svc.common.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author zzq
+ */
+public interface IdGenerator {
+
+    public Long generateId();
+}
