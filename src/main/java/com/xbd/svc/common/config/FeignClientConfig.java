330215d1e89988b1566b3a72be11c9f613882a96
zhouziqi <jazhouzq@163.com>
2019-05-17 11:53:25 +0800

    增加了FeignP配置

--- src/main/java/com/xbd/svc/common/config/FeignClientConfig.java
+++ src/main/java/com/xbd/svc/common/config/FeignClientConfig.java
@@ -2,7 +2,12 @@ package com.xbd.svc.common.config;
 
 import feign.Logger;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.web.WebMvcRegistrations;
+import org.springframework.boot.autoconfigure.web.WebMvcRegistrationsAdapter;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @Slf4j
 public class FeignClientConfig {
@@ -10,5 +15,26 @@ public class FeignClientConfig {
     @Bean
     public Logger.Level feignLoggerLevel(){
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    public WebMvcRegistrations feignWebRegistrations() {
+        return new WebMvcRegistrationsAdapter() {
+            @Override
+            public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
+                return new FeignRequestMappingHandlerMapping();
+            }
+        };
+    }
+
+    /**
+     * 不能被@FeignClient注解修饰的类才会进行解析RequestMapping
+     */
+    private static class FeignRequestMappingHandlerMapping extends RequestMappingHandlerMapping {
+        @Override
+        protected boolean isHandler(Class<?> beanType) {
+            return super.isHandler(beanType) &&
+                    !AnnotatedElementUtils.hasAnnotation(beanType, FeignClient.class);
+        }
     }
 }
